b51c9a056c58c8c57263e05af64fae9aadcf2604
Emma Alfaro <gabrielaa027@gmail.com>
2022-10-17 14:34:01 +0000

    Practica 3

--- tomay.c
+++ tomay.c
@@ -8,6 +8,9 @@ int main(){
     do{
         n = read(STDIN_FILENO, &c, 1);
         may = toupper(c);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            may = '1';
+        }
         write(STDOUT_FILENO, &may, 1);
     }
     while(n != 0);
