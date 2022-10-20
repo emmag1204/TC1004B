bfb6d77569eb75d3e1fac8340c8641488624712a
Emma Alfaro <gabrielaa027@gmail.com>
2022-10-20 13:06:22 +0000

    practica 5

--- thread.c
+++ thread.c
@@ -1,26 +1,44 @@
 #include <pthread.h>
 #include <stdio.h>
+#include <semaphore.h>
+#define NUM_THREADS 10
 
-typedef struct thread_data{
+/*ypedef struct thread_data{
     int threadId;
     char name[20];
+    //..
 } ThreadData;
-typedef char caracter;
+typedef char caracter;*/
 
-void * holaMundo(void *arg){
-    ThreadData *myData = (ThreadData *)arg;
-    printf("Hola desde el hilo\n");
+int saldo;
+sem_t protectSaldo;
+
+void * suma(void *arg){
+    sem_wait(&protectSaldo); // decrementa
+    printf("El saldo inicial es %d\n", saldo);
+    saldo = saldo + 100;
+    printf("El saldo despues es %d\n", saldo);
+    sem_post(&protectSaldo); // incrementa
     pthread_exit(NULL);
 }
+/*void * holaMundo(void *arg){
+    ThreadData *myData = (ThreadData *)arg;
+    printf("Hola desde el hilo y mi id es %d\n", myData->threadId);
+    pthread_exit(NULL);
+}*/
 
 int main(){
-    for (int i = 0; i < 100; i++){
-        ThreadData myData;
-        pthread_t threadId;
-        myData.threadId = i;
-        pthread_create(&threadId, NULL, holaMundo, NULL);
+    saldo = 0;
+    pthread_t threadsInfo[NUM_THREADS];
+    sem_init(&protectSaldo, 0, 1);
+    //Create threads
+    for (int i = 0; i < NUM_THREADS; i++){
+        pthread_create(&threadsInfo[i], NULL, suma, NULL);
     }
+    for (int i =  0; i < NUM_THREADS; i++){
+        pthread_join(threadsInfo[i], NULL);
+    }
+    printf("El final es %d\n", saldo);
     pthread_exit(NULL);
     printf("NUNCA LLEGA");
-
 }
